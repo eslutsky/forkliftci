633ce57632236aca659c47a3c1d7673b5569ebdd
Evgeny Slutsky <eslutsky@redhat.com>
2022-11-23 15:00:13 +0200

    test with custom build
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- get_forklift_bazel.sh
+++ get_forklift_bazel.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "Running $0"
 
-git clone git@github.com:kubev2v/forklift.git
+#git clone git@github.com:kubev2v/forklift.git
+git clone git@github.com:eslutsky/forklift.git -b remove-kvm-req
